01ac59d7ae30210bf91f71bbd54e4980a19b9e5f
Marc Ivan Guillermo <61181505+aerahrah@users.noreply.github.com>
2025-08-13 15:45:48 +0800

    add design in login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';         
-import Dashboard from './pages/Dashboard';  
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,26 +1,20 @@
-import { useState } from 'react';
-import { useMutation } from '@tanstack/react-query';
-import { loginAdmin } from '../api/admin';
-import { useAuth } from '../store/authStore';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { loginAdmin } from "../api/admin";
+import { useAuth } from "../store/authStore";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const { login } = useAuth();
   const navigate = useNavigate();
-  const [form, setForm] = useState({ username: '', password: '' });
+  const [form, setForm] = useState({ username: "", password: "" });
 
-  const {
-    mutate,
-    isPending,
-    isSuccess,
-    isError,
-    error
-  } = useMutation({
+  const { mutate, isPending, isSuccess, isError, error } = useMutation({
     mutationFn: loginAdmin,
     onSuccess: (data) => {
       login(data); // store admin data in context
-      navigate('/dashboard');
-    }
+      navigate("/dashboard");
+    },
   });
 
   const handleChange = (e) =>
@@ -32,34 +26,55 @@ const Login = () => {
   };
 
   return (
-    <div style={{ maxWidth: '400px', margin: 'auto', padding: '2rem' }}>
-      <h2 className='text-red-500'>Admin Login</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          className='text-red-500'
-          name="username"
-          placeholder="Username"
-          value={form.username}
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          value={form.password}
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <button type="submit" disabled={isPending}>
-          {isPending ? 'Logging in...' : 'Login'}
-        </button>
-      </form>
+    <div className="relative size-dvh w-dvw bg-neutral-200">
+      <div className="absolute top-[50%] right-[50%] translate-y-[-50%] translate-x-[50%] bg-neutral-50 p-10 rounded-sm">
+        <div className="my-4">
+          <h1 className="text-3xl font-semibold">Employee Management System</h1>
+          <h2 className="text-xl">Login to your Admin Account</h2>
+        </div>
 
-      {isError && <p style={{ color: 'red' }}>{error.response?.data?.message || 'Login failed'}</p>}
-      {isSuccess && <p style={{ color: 'green' }}>✅ Login successful</p>}
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-1">
+            <label>Username</label>
+            <input
+              className="min-w-96  w-full border-1 rounded-sm p-2.5 border-neutral-300 focus:outline-none focus:border-neutral-500"
+              name="username"
+              placeholder="Username"
+              value={form.username}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <label>Password</label>
+            <input
+              className="min-w-96  w-full border-1 rounded-sm p-2.5 border-neutral-300 focus:outline-none focus:border-neutral-500"
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={form.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <button
+            className="bg-neutral-700 text-neutral-50 w-full p-3 mt-3 rounded-sm"
+            type="submit"
+            disabled={isPending}
+          >
+            {isPending ? "Logging in..." : "Login"}
+          </button>
+        </form>
+
+        {isError && (
+          <p style={{ color: "red" }}>
+            {error.response?.data?.message || "Login failed"}
+          </p>
+        )}
+        {isSuccess && <p style={{ color: "green" }}>✅ Login successful</p>}
+      </div>
     </div>
   );
 };
